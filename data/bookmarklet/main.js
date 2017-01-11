dc6c8e9567498b0c2eba5e5495ad62f67bb43db4
mizt <info@mizt.org>
2017-01-11 12:23:09 +0900

    updated

--- data/bookmarklet/main.js
+++ data/bookmarklet/main.js
@@ -7,17 +7,20 @@
 	
 	if(!!t) {
 		var c = d.createElement("canvas");
-		c.width = "656"; c.height = "368";
+		c.width = w; c.height = h;
 		var e = c.getContext("2d");
 		d.body.appendChild(c);
 		var s = c.style;
-		s.zIndex=242;
+		s.zIndex=10000000000;
 		s.position="absolute";
 		s.top = 0;
+		s.width = w;
+		s.height = h;
 		var m = new Uint8Array(Module.HEAPU8.buffer,Module._malloc(r,r));	
 		(Module.cwrap("setup","void",["number"]))(w,h);	
 		setInterval(function(){
-			e.drawImage(t,0,0);
+			var r = t.getBoundingClientRect();
+			e.drawImage(t,0,0,r.width,r.height,0,0,w,h);
 			var i = e.getImageData(0,0,w,h);
 			m.set(new Uint8Array(i.data.buffer));
 			(Module.cwrap("draw","void",["number"]))(m.byteOffset);
